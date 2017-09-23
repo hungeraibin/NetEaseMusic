5860b97ef503417da61263383d9ea7bae2dedab9
aibin <1149299088@qq.com>
2017-09-23 19:49:19 +0800

    完善歌词滚动 高亮

--- song.js
+++ song.js
@@ -5,7 +5,7 @@ $(function () {
         let songs = response;
         let song = songs.filter((s)=>{
             return (s.id == id);
-        })[0]
+        })[0];
         let {url, name, lyric} = song;
         initPlayer.call(undefined, url);
         initText(name, lyric);
@@ -31,6 +31,34 @@ $(function () {
             audio.play();
             $('.dist-container').addClass('playing');
         })
+        setInterval(()=>{
+            let seconds = audio.currentTime;
+            let minutes = ~~(seconds / 60);
+            let left = seconds - minutes * 60;
+            let time = `${pad(minutes)}:${pad(left)}`;
+            let $lines = $('.lines > p');
+            let $whichLine;
+            for (let i = 0; i < $lines.length; i++) {
+                let currentLineTime = $lines.eq(i).attr('data-time');
+                let nextLineTime = $lines.eq(i+1).attr('data-time');
+                if  ($lines.eq(i+1).length !== 0 && currentLineTime< time && nextLineTime > time) {
+                    $whichLine = $lines.eq(i);
+                    break;
+                }
+            }
+            if ($whichLine) {
+                $whichLine.addClass('active').prev().removeClass('active');
+                let top = $whichLine.offset().top;
+                let linesTop = $('.lines').offset().top;
+                let delta = top - linesTop - $('.lyric').height()/3;
+                console.log(delta)
+                $('.lines').css('transform', `translateY(-${delta}px)`);
+            }
+        }, 300);
+    }
+    
+    function pad(number) {
+        return number>=10 ? number + '' : '0' + number;
     }
 
     function parseLyric(lyric) {
